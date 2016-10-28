ab5a0596d071e78413b66e09455ff59a9c43fc80
root <jinxiaofeng211@126.com>
2016-10-28 18:41:27 +0800

    api测试通过

--- nspdhcpagent.py
+++ nspdhcpagent.py
@@ -20,46 +20,6 @@ from nspagent.dhcp.linux import daemon
 
 LOG = logging.getLogger(__name__)
 
-def daemonize(stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
-    """
-    do the UNIX double-fork magic, see Stevens' "Advanced
-    Programming in the UNIX Environment" for details (ISBN 0201563177)
-    http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
-    """
-    try:
-        pid = os.fork()
-        if pid > 0:
-            # exit first parent
-            sys.exit(0)
-    except OSError, e:
-        sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
-        sys.exit(1)
-
-    # decouple from parent environment
-    os.chdir("/")
-    os.setsid()
-    os.umask(0)
-
-    # do second fork
-    try:
-        pid = os.fork()
-        if pid > 0:
-            # exit from second parent
-            sys.exit(0)
-    except OSError, e:
-        sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
-        sys.exit(1)
-
-    # redirect standard file descriptors
-    sys.stdout.flush()
-    sys.stderr.flush()
-    si = file(stdin, 'r')
-    so = file(stdout, 'a+')
-    se = file(stderr, 'a+', 0)
-    os.dup2(si.fileno(), sys.stdin.fileno())
-    os.dup2(so.fileno(), sys.stdout.fileno())
-    os.dup2(se.fileno(), sys.stderr.fileno())
-
 def register_options():
     config.register_interface_driver_opts_helper(cfg.CONF)
     config.register_use_namespaces_opts_helper(cfg.CONF)
@@ -116,19 +76,9 @@ if __name__== '__main__':
     common_config.init(sys.argv[1:])
     config.setup_logging()
 
-    #parser = argparse.ArgumentParser()
-    #parser.add_argument("-d", "--daemon", help="run in background",
-    #                    action="store_true")
-
-    #args = parser.parse_args()
-    #if args.daemon and os.getppid() != 1:
-    #    daemonize()
-
     signal.signal(signal.SIGTERM, sigterm_handler)
 
-   # init_logger(args)
     LOG.info('Launching DhcpAgent API Stack (DHCP_AGENT) ...')
-
 
     #local_ctrl_ip = get_ip_address("nspbr0")
     local_ctrl_ip = "192.168.49.22"
